c2152c1a0e6f059dde0e7cf174ce1d0c86800607
FranRom <maildefranz@gmail.com>
2017-10-30 18:20:07 +0100

    Fluid movement with friction added, limits with bounce effect.

--- JS/Player.js
+++ JS/Player.js
@@ -5,16 +5,17 @@ function Player(x, y, speed, radius, ctx, color) {
   this.x = x;
   this.y = y;
   this.speed = speed;
-  this.direction = [false,false,false,false];
   this.radius = radius;
+  this.Vx = 0;
+  this.Vy = 0;
+  this.friction = 0.9;
   this.ctx = ctx;
   this.color = color;
-
+  this.keys = {};
 };
 
 // Draw player
 Player.prototype.draw = function () {
-
   this.ctx.beginPath();
   this.ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
   this.ctx.strokeStyle = this.color;
@@ -23,37 +24,55 @@ Player.prototype.draw = function () {
   this.ctx.fill();
 };
 
-// Moving functions
-
+//Moving functions
 Player.prototype.moveLeft = function () {
-  if (this.x > this.radius){
-    this.x -= this.speed;
+  if (this.Vx > -this.speed) {
+      this.Vx--;
   }
-  console.log("Left was called");
+  console.log("moveLeft was called");
 };
-//si cambio 600 por canvas.width no funciona, lo mismo con canvas.height
+
 Player.prototype.moveRight = function () {
-  if (this.x > 0 && this.x < 600 - this.radius){
-    this.x += this.speed;
+  if (this.Vx < this.speed) {
+      this.Vx++;
   }
-  console.log("Right was called");
+  console.log("moveRight was called");
 };
 
 Player.prototype.moveUp = function () {
-  if (this.y > this.radius){
-    this.y -= this.speed;
+    if (this.Vy > -this.speed) {
+        this.Vy--;
   }
-
-  console.log("Up was called");
+  console.log("moveUp was called");
 };
 
 Player.prototype.moveDown = function () {
-  if (this.y > 0 && this.y < 600 - this.radius){
-    this.y += this.speed;
-  }
-  console.log("Down was called");
+    if (this.Vy < this.speed) {
+        this.Vy++;
+    }
+  console.log("moveDown was called");
 };
 
+Player.prototype.updatePosition = function() {
+  this.Vy *= this.friction;
+  this.y  += this.Vy;
+  this.Vx *= this.friction;
+  this.x  += this.Vx;
+  if(this.y + this.radius <= 50) {
+     this.y = 25 + this.radius;
+  };
+  if(this.y + this.radius > 600) {
+     this.y = 575 - this.radius;
+   };
+  if(this.x + this.radius <= 50) {
+     this.x = 25 + this.radius;
+  }
+  if(this.x + this.radius > 600) {
+     this.x = 575 - this.radius;
+  }
+
+
+};
 
 //Punch Function
 Player.prototype.punch = function () {
--- JS/main.js
+++ JS/main.js
@@ -1,3 +1,5 @@
+
+
   var canvas = document.getElementById('ring');
   var ctx = canvas.getContext('2d');
   //we can pass a "color" argument to canvas color here: now transparent
@@ -9,88 +11,35 @@
 
 //Clean the canvas and draw canvas and player again
 function updateGameArea() {
- ctx.clearRect(0, 0, 690, 690);
- myGameArea.draw();
- player.draw();
+ requestAnimationFrame(updateGameArea);
+    //UP
+    if (player.keys[38]) {
+        player.moveUp();
+    }
+    //DOWN
+    if (player.keys[40]) {
+        player.moveDown();
+    }
+    //RIGHT
+    if (player.keys[39]) {
+        player.moveRight();
+    }
+    //LEFT
+    if (player.keys[37]) {
+        player.moveLeft();
+    }
+    ctx.clearRect(0, 0, 690, 690);
+    myGameArea.draw();
+    player.updatePosition();
+    player.draw();
 };
 
-// Keyboard detection
-// $(document).keydown (function(e) {
-//  switch (e.which) {
-//    case 38:
-//     player.moveUp();
-//     console.log('up', player);
-//     break;
-//
-//    case 40:
-//     player.moveDown();
-//    console.log('down', player);
-//     break;
-//
-//    case 37:
-//     player.moveLeft();
-//     console.log('left', player);
-//     break;
-//
-//    case 39:
-//     player.moveRight();
-//    console.log('right', player);
-//     break;
-//  }
-//  updateGameArea();
-// });
-
-$(document).keydown (function(e) {
-  if (e.keyCode == 39) {
-    player.direction[0] = true;
-  } else if (e.keyCode == 37) {
-    player.direction[1] = true;
-  } else if (e.keyCode == 38) {
-    player.direction[2] = true;
-  } else if (e.keyCode == 40) {
-    player.direction[3] = true;
-  }
-  console.log(player.direction);
-  player.updatePlayer();
-  updateGameArea();
+document.body.addEventListener("keydown", function (e) {
+  player.keys[e.keyCode] = true;
 
 });
-
-$(document).keyup (function(e) {
-  if (e.keyCode == 39) {
-    player.direction[0] = false;
-  } else if (e.keyCode == 37) {
-    player.direction[1] = false;
-  } else if (e.keyCode == 38) {
-    player.direction[2] = false;
-  } else if (e.keyCode == 40) {
-    player.direction[3] = false;
-  }
+document.body.addEventListener("keyup", function (e) {
+  player.keys[e.keyCode] = false;
 });
 
-Player.prototype.updatePlayer = function() {
-  if (this.direction[2] == true && this.direction[0] == true) {
-    this.moveUp();
-    this.moveRight();
-  } else if (this.direction[3] == true && this.direction[1] == true) {
-    this.moveDown();
-    player.moveLeft();
-  } else if (this.direction[2] == true && this.direction[1] == true) {
-    this.moveUp();
-    this.moveLeft();
-  } else if (this.direction[3] == true && this.direction[0] == true) {
-    this.moveDown();
-    this.moveRight();
-  } else if (this.direction[2] == true) {
-    player.moveUp();
-  } else if (this.direction[3] == true) {
-    player.moveDown();
-  } else if (this.direction[0] == true) {
-    player.moveRight();
-  } else if (this.direction[1] == true) {
-    player.moveLeft();
-  }
-  console.log("move function was called");
-};
-
-//Updating
+updateGameArea();
